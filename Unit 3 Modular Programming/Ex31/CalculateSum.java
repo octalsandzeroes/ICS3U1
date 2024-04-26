fe548fa750efbdb7a659c63c85a69e51880dc36b
Null1fied <80koftheend@gmail.com>
2024-04-26 12:21:11 -0400

    2024-04-26 12:20PM Completed Ex31

---
+++ Unit 3 Modular Programming/Ex31/CalculateSum.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+/*
+CalculateSum
+By Andrew Martinus
+Last modified on April 25, 2024
+This program prompts the user for a range of values and prints the sum of integers within the range
+*/
+
+public class CalculateSum{
+    // main method
+    public static void main(String[] args){
+        // creates the scanner
+        Scanner sc = new Scanner(System.in);
+        int from, to;
+
+        // recieves a user input for the bounds
+        System.out.print("Enter the left side bound: ");
+        from = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter the right side bound: ");
+        to = Integer.parseInt(sc.nextLine());
+        System.out.printf("The sum of the integers between %d to %d is %d", from, to, sumRange(from, to));
+        
+    }
+
+    /*====================================================================
+    |  int sumRange(int from, int to)                                    |
+    |--------------------------------------------------------------------|
+    |  returns int - The sum of the integers in the range (inclusive)    |
+    |--------------------------------------------------------------------|
+    |  int from - This parameter is the left side integer bound          |
+    |--------------------------------------------------------------------|
+    |  int to - This parameter is the right side integer bound           |
+    |--------------------------------------------------------------------|
+    |  This method takes a left side and right side bound of integers    |
+    |  and then prints the sum of the integers between them (inclusive). |
+    ====================================================================*/
+    public static int sumRange(int from, int to){
+        int sum = 0;
+        // runs a for loop to sum up the values
+        if (to > from){
+            for (int i = from; i <= to; i++){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
---
+++ Unit 3 Modular Programming/Ex31/CheckDivisibility.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+/*
+CheckDivisibility
+By Andrew Martinus
+Last modified on April 25, 2024
+This program prompts the user for two values and prints out if the first is evenly divisible by the second
+*/
+
+public class CheckDivisibility{
+    // main method
+    public static void main(String[] args){
+        // creates the scanner
+        Scanner sc = new Scanner(System.in);
+        int num, den;
+
+        // recieves a user input for the dividend an divisor
+        System.out.print("Enter the numerator: ");
+        num = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter the denominator: ");
+        den = Integer.parseInt(sc.nextLine());
+        if (evenlyDivisible(num, den)){
+            System.out.printf("%d is perfectly divisible by %d", num, den);
+        } else {
+            System.out.printf("%d is not perfectly divisible by %d", num, den);
+        }
+    }
+
+    /*====================================================================
+    |  boolean evenlyDivisible(int from, int to)                         |
+    |--------------------------------------------------------------------|
+    |  returns boolean - The boolean for whether it is evenly divisible  |
+    |--------------------------------------------------------------------|
+    |  int num - This parameter is the dividend                          |
+    |--------------------------------------------------------------------|
+    |  int den - This parameter is the divisor                           |
+    |--------------------------------------------------------------------|
+    |  This method checks whether a number is evenly divisible and then  |
+    |  returns true or false                                             |
+    ====================================================================*/
+    public static boolean evenlyDivisible(int num, int den){
+        boolean check = false;
+        // runs a for loop to sum up the values
+        if (num % den == 0){
+            check = true;
+        }
+        return check;
+    }
+}
