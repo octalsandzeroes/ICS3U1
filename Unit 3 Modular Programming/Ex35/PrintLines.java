6e458c707943260384f73a4ae4d35a68834828e3
Null1fied <80koftheend@gmail.com>
2024-05-05 23:36:09 -0400

    TEMP

---
+++ Unit 3 Modular Programming/Ex35/PrintLines.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/*
+PrintLines
+By Andrew Martinus
+Last modified on May 5, 2024
+This program reads and prints out each line from a file called "line.txt"
+*/
+
+public class PrintLines{
+    // main method
+    public static void main(String[] args){
+        
+    }
+}
