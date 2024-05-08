dc6f47e7fc663ef60d6d658e41ea12f806f1d638
Null1fied <80koftheend@gmail.com>
2024-05-07 22:06:15 -0400

    2024-05-07 10:06PM Ex36++

--- Unit 3 Modular Programming/Ex36/StudentMarks.java
+++ Unit 3 Modular Programming/Ex36/StudentMarks.java
@@ -5,7 +5,7 @@ import java.util.*;
 StudentMarks
 By Andrew Martinus
 Last modified on May 5, 2024
-This program 
+This program reads student data on tests from a file and writes onto another file various averages and rankings
 */
 
 public class StudentMarks {
@@ -18,20 +18,17 @@ public class StudentMarks {
         Scanner sc = new Scanner(System.in);
         String[][] scores;
         String lineIn;
-        int students, tests, charIn, counter = 0, bestStudent = -1;
+        int students = 0, tests = 0, charIn, counter = 0, bestStudent = -1;
         double highest = 0, average = 0;
 
 
         // initializes and assigns values to the test score array and writes to a second file the averages of the tests for each student
         try {
-            BufferedReader sourceIn = new BufferedReader(new FileReader(FILE_NAME1));
-
-            BufferedReader resultIn = new BufferedReader(new FileReader(FILE_NAME2));
-            BufferedWriter resultOut = new BufferedWriter(new FileWriter(FILE_NAME2, true));
-
+            BufferedReader in1 = new BufferedReader(new FileReader(FILE_NAME1));
+            BufferedWriter out1 = new BufferedWriter(new FileWriter(FILE_NAME2, true));
             // initializes the 2D array with integer values from the file
-            students = Integer.parseInt(sourceIn.readLine());
-            tests = Integer.parseInt(sourceIn.readLine());
+            students = Integer.parseInt(in1.readLine());
+            tests = Integer.parseInt(in1.readLine());
             scores = new String[students][tests];
             
             // reads each character from the file in focus and appends characters not seperated by '\n' or ' ' to each index in the array
@@ -39,7 +36,7 @@ public class StudentMarks {
                 for (int j = 0; j < tests; j++){
                     scores[i][j] = "";
                     do{
-                        charIn = sourceIn.read();
+                        charIn = in1.read();
                         if ((char) charIn != ' ' && (char) charIn != '\n' && charIn != -1){
                             scores[i][j] += (char) charIn;
                         }
@@ -67,43 +64,54 @@ public class StudentMarks {
                 }
                 average /= tests;
                 // System.out.printf("The average of student %d is %.2f%n", i+1, average);
-                resultOut.write(String.format("%.2f", average) + "\n");
+                out1.write(String.format("%.2f", average) + "\n");
                 average = 0;
             }
             
+            // closes the reader and saves the material in the datastream to the output file
+            in1.close();
+            out1.close();
+
+            BufferedReader in2 = new BufferedReader(new FileReader(FILE_NAME2));
+            BufferedWriter out2 = new BufferedWriter(new FileWriter(FILE_NAME2, true));
             // writes the student number of the student with the highest average
-            lineIn = resultIn.readLine();
-            while (counter <= students){
+            lineIn = in2.readLine();
+            System.out.println(lineIn);
+            while (counter < students){
                 // System.out.println("lineIn: " + Double.parseDouble(lineIn) + " highest:" + highest);
                 counter++;
                 if (Double.parseDouble(lineIn) > highest){
                     highest = Double.parseDouble(lineIn);
                     bestStudent = counter;
                 }
-                lineIn = resultIn.readLine();
+                lineIn = in2.readLine();
                 System.out.println(counter);
             }
-            resultOut.write(bestStudent + "\n");
-
+            out2.write(bestStudent + "\n");
+            
+            // writes the class average for each test
             for (int i = 0; i < tests; i++){
                 for (int j = 0; j < students; j++){
                     average += Double.parseDouble(scores[j][i]);
                 }
                 average /= students;
-                resultOut.write(String.format("%.2f", average) + "\n");
+                out2.write(String.format("%.2f", average) + "\n");
             }
 
-            resultIn.close();
-            resultOut.close();
-            sourceIn.close();
+            // closes the reader and saves the material in the datastream to the output file
+            in2.close();
+            out2.close();
+
         } catch (IOException e){
-            System.out.println("Problem interacting with " + e.getMessage());
+            System.out.println("Problem interacting with file:" + e.getMessage());
         } catch (NumberFormatException f) {
-            System.out.println("Invalid input detected, please check input file " + FILE_NAME1);
+            System.out.println("Invalid input detected, please check input file " + f.getMessage());
         } catch (Exception g){
             System.out.println("An error occured!: " + g);
             g.printStackTrace();
         }
+
+        System.out.println(tests);
 
         sc.close();
     }
---
+++ Unit 3 Modular Programming/Ex36/StudentMarksExperiments.java
@@ -0,0 +1,160 @@
+import java.io.*;
+import java.util.*;
+
+/*
+StudentMarksExperiments
+By Andrew Martinus
+Last modified on May 5, 2024
+This program aims to tinker with using BufferedReader and BufferedWriter in methods
+*/
+
+public class StudentMarksExperiments {
+    public static void main(String[] args) {
+        // static variable for the file in focus and the times table upper bound
+        final String SOURCE_FILE = "marks.txt";
+        final String OUTPUT_FILE = "result.txt";
+
+        // declares the the array and size variables and creates the scanner objects
+        Scanner sc = new Scanner(System.in);
+        String[][] scores;
+        String lineIn;
+        int students = 0, tests = 0, charIn, counter = 0, bestStudent = -1;
+        double highest = 0, average = 0;
+
+        try {
+            BufferedReader sourceIn = new BufferedReader(new FileReader(SOURCE_FILE));
+            BufferedReader outputIn = new BufferedReader(new FileReader(OUTPUT_FILE));
+        } catch (IOException e) {
+            System.out.println("Error accessing file " + e.getMessage());
+        }
+
+        // initializes and assigns values to the test score array and writes to a second file the averages of the tests for each student   
+        // initializes the 2D array with integer values from the file
+        students = Integer.parseInt(autobufferedread(sourceIn, true));
+        tests = Integer.parseInt(autobufferedread(sourceIn, true));
+        System.out.println("Students: " + students + " Tests: " + tests);
+        scores = new String[students][tests];
+        
+        // reads each character from the file in focus and appends characters not seperated by '\n' or ' ' to each index in the array
+        for (int i = 0; i < students; i++){
+            for (int j = 0; j < tests; j++){
+                scores[i][j] = "";
+                do{
+                    charIn = Integer.parseInt(autobufferedread(sourceIn, false));
+                    if ((char) charIn != ' ' && (char) charIn != '\n' && charIn != -1){
+                        scores[i][j] += (char) charIn;
+                    }
+                } while ((char) charIn != ' ' && (char) charIn != '\n' && charIn != -1);
+            }
+        }
+
+        /* prints the 2D array
+        System.out.println();
+        for (int i = 0; i < students; i++){
+            for (int j = 0; j < tests; j++){
+                if (j == tests-1){
+                    System.out.println(scores[i][j]);    
+                } else {
+                    System.out.print(scores[i][j] + " ");
+                }
+            }
+        }
+        */
+
+
+        // calculates the averages for each student and writes them to a seperate file
+        for (int i = 0; i < students; i++){
+            for (int j = 0; j < tests; j++){
+                average += Double.parseDouble(scores[i][j]);
+            }
+            average /= tests;
+            // System.out.printf("The average of student %d is %.2f%n", i+1, average);
+            autobufferedwrite(String.format("%.2f", average) + "\n", OUTPUT_FILE, true);
+            average = 0;
+        }
+
+
+        // writes the student number of the student with the highest average
+        lineIn = autobufferedread(outputIn, true);
+        System.out.println(lineIn);
+        while (counter < students){
+            // System.out.println("lineIn: " + Double.parseDouble(lineIn) + " highest:" + highest);
+            counter++;
+            if (Double.parseDouble(lineIn) > highest){
+                highest = Double.parseDouble(lineIn);
+                bestStudent = counter;
+            }
+            lineIn = autobufferedread(outputIn, true);
+            System.out.println(counter);
+        }
+        autobufferedwrite(bestStudent + "\n", OUTPUT_FILE, true);
+        
+        
+        // prints the class averages for each test
+        for (int i = 0; i < tests; i++){
+            for (int j = 0; j < students; j++){
+                average += Double.parseDouble(scores[j][i]);
+            }
+            average /= students;
+            autobufferedwrite(String.format("%.2f", average) + "\n", OUTPUT_FILE, true);
+        }
+
+
+
+        sc.close();
+    }
+
+    /*===============================================================================
+    |  void autobufferedwrite(String output, String FILE_NAME, boolean appendState) |
+    |-------------------------------------------------------------------------------|
+    |  String output - The data the user wishes to write to a file                  |
+    |-------------------------------------------------------------------------------|
+    |  String FILE_NAME - The file the user wishes to write to                      |
+    |-------------------------------------------------------------------------------|
+    |  boolean appendState - The boolean to enable or disable appending             |
+    |-------------------------------------------------------------------------------|
+    |  This method writes to a file in focus a user input                           |
+    ===============================================================================*/
+    public static void autobufferedwrite(String output, String FILE_NAME, boolean appendState){
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(FILE_NAME, appendState));
+            out.write(output);
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Problem writing " + e.getMessage());
+        } catch (Exception f){
+            System.out.println("An error occured!: " + f);
+            f.printStackTrace();
+        }
+    }
+
+    /*===============================================================================
+    |  String autobufferedread(String FILE_NAME, boolean readLine)                  |
+    |-------------------------------------------------------------------------------|
+    |  Returns String - Returns read data                                           |
+    |-------------------------------------------------------------------------------|
+    |  BufferedReader in - The BufferedReader object to be used                     |
+    |-------------------------------------------------------------------------------|
+    |  boolean readLine - The boolean defining the read type to be executed         |
+    |-------------------------------------------------------------------------------|
+    |  This method reads from a file in focus                                       |
+    ===============================================================================*/
+    public static String autobufferedread(BufferedReader in, boolean readLine){
+        String readData = null;
+
+        try {
+            if (readLine == false){
+                readData = Character.toString((char) in.read());
+            } else {
+                readData = in.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Problem reading " + e.getMessage());
+        } catch (Exception f){
+            System.out.println("An error occured!: " + f);
+            f.printStackTrace();
+        }
+
+        return readData;
+    }
+}
