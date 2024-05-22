7e66259edb10cd6e448ab880e91484d7240d9091
Null1fied <80koftheend@gmail.com>
2024-05-22 15:15:33 -0400

    2024-05-22 3:15PM Case Format

--- Unit 4 Software Engineering/Implementation/HotelOS.java
+++ Unit 4 Software Engineering/Implementation/HotelOS.java
@@ -7,7 +7,19 @@ public class HotelOS {
         final String RESERVATIONS_LIST = "reservationslist.txt";
         final String ROOMS_LIST = "roomslist.txt";
         final String ADMIN_ID = "000000";
-        final String ADMIN_OPTIONS = "1) List the available rooms for a given date\n" + //
+        final String ADMIN_OPTIONS = "\n1) List the available rooms for a given date\n" + //
+                                    "2) List all the reservations for a given date\n" + //
+                                    "3) Look up all the reservations made under a certain name\n" + //
+                                    "4) Make a reservation for a room\n" + //
+                                    "5) Cancel a reservation for a room\n" + //
+                                    "6) Change the details on a reservation\n" + //
+                                    "7) Change PIN number\n" + //
+                                    "8) Add a hotel room\n" + //
+                                    "9) Delete a hotel room\n" + //
+                                    "10) Add an employee\n" + //
+                                    "11) Delete an employee\n" + //
+                                    "0) Log out\n";
+        final String GENERAL_OPTIONS = "\n1) List the available rooms for a given date\n" + //
                                     "2) List all the reservations for a given date\n" + //
                                     "3) Look up all the reservations made under a certain name\n" + //
                                     "4) Make a reservation for a room\n" + //
@@ -19,8 +31,12 @@ public class HotelOS {
         Scanner sc = new Scanner(System.in);
         
         int loginType = 0;
+        int userInput = -1;
         String loginString;
         String employeeName;
+        boolean validOption = false;
+        boolean taskDone = true;
+
 
         while (true){
             do {
@@ -28,41 +44,41 @@ public class HotelOS {
                 loginType = Integer.parseInt(loginString.split("\\|", 4)[0]); // | is a metacharacter in regex, so it has to be escape charactered
                 employeeName = loginString.split("\\|", 4)[3];
             } while (loginType == 0);
-/*
-            try {  
-                BufferedReader in = new BufferedReader(new FileReader(EMPLOYEE_LIST));
-                int counter = 0;
-                String lineIn = in.readLine();
-                while (lineIn != null){
-                    counter++;
-                    lineIn = in.readLine();
-                }
-                String[] strarr = new String[counter];
-                counter = 0;
-                in.close();
 
-                BufferedReader in2 = new BufferedReader(new FileReader(EMPLOYEE_LIST));
-                lineIn = in2.readLine();
-                while (counter < strarr.length){
-                    strarr[counter] = lineIn;
-                    lineIn = in2.readLine();
-                    counter++;
-                }
-                
-                for (int i = 0; i < strarr.length; i++){
-                    System.out.println(strarr[i]);
-                }
-                in2.close();
-            } catch (IOException e){
-                System.out.println("E");
-            }
-*/
             while (loginType == 1){
-                
+                if (taskDone == true){
+                    System.out.println(ADMIN_OPTIONS);
+                    System.out.print("Enter a numeric option: ");
+                    taskDone = false;
+                }
+
+                while (validOption == false) {
+                    try {
+                        userInput = Integer.parseInt(sc.nextLine());
+                        validOption = true;
+                    } catch (NumberFormatException e){
+                        System.out.print("Invalid option, please retry: ");
+                    }
+                }
+
+                switch (userInput) {
+                    case 1:
+                        System.out.println(getname(sc));
+                        taskDone = true;
+                        validOption = false;
+                        break;
+                    case 0:
+                        loginType = 0;
+                        break;
+                    default:
+                        System.out.print("Invalid option, please retry: ");
+                        validOption = false;
+                        break;
+                }
             }
 
             while (loginType == 2){
-                
+                System.out.println(GENERAL_OPTIONS);
             }
         }
     }
@@ -123,6 +139,49 @@ public class HotelOS {
         return nameString;
     }
 
+
+    public static String getdate (Scanner sc){
+        String[] dateCheckArray;
+        String date = null;
+        boolean validDate = false;
+
+        System.out.print("Enter a date in the \"DD/MM/YYYY\" format: ");
+        while (validDate == false){
+            date = sc.nextLine();
+            dateCheckArray = date.split("/", 0);
+            // checks the formatting
+            if (dateCheckArray.length == 3){
+                // checks that the entered date is valid
+                try {
+                    if (Integer.parseInt(dateCheckArray[0]) >= 1 && Integer.parseInt(dateCheckArray[0]) <= 31 && 
+                    Integer.parseInt(dateCheckArray[1]) >= 1 && Integer.parseInt(dateCheckArray[1]) <= 12 && 
+                    Integer.parseInt(dateCheckArray[2]) >= 2024 && Integer.parseInt(dateCheckArray[2]) <= 2054){
+                            validDate = true;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.print("Invalid date, please retry: ");
+                }
+            }
+            if (validDate == false) System.out.print("Invalid date, please retry: ");
+        }
+
+        return date;
+    }
+
+
+    /*============================================================================
+    |  String employeelogin(Scanner sc, String EMPLOYEE_LIST, String ADMIN_ID)   |
+    |----------------------------------------------------------------------------|
+    |  Scanner sc - The System.in scanner object                                 |
+    |----------------------------------------------------------------------------|
+    |  String EMPLOYEE_LIST - The string with the employee list file string      |
+    |----------------------------------------------------------------------------|
+    |  String ADMIN_ID - The string with admin user ID                           |
+    |----------------------------------------------------------------------------|
+    |  returns String - A login code string                                      |
+    |----------------------------------------------------------------------------|
+    |  This method prompts for a employee login until it is successful           |
+    ============================================================================*/
     public static String employeelogin(Scanner sc, String EMPLOYEE_LIST, String ADMIN_ID){
         final String exitCode = "0";
         
@@ -206,4 +265,7 @@ public class HotelOS {
 
         return loginString;
     }
+
+
+
 }
